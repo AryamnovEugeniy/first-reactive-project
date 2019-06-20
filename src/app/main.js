860e54e26fe363dbb517a422eb51ed548f5d7ab3
Eugeniy Aryamnov (DevExpress) <aryamnov.eugeniy@corp.devexpress.com>
2019-06-20 11:05:05 +0300

    Created two-page site

---
+++ src/app/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Main(props) {
+    return (
+        <h1>Hello!</h1>
+        
+    );
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Main from './app/main';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Route exact path="/" component={App} />
+        <Route path="/main" component={Main} />
+    </Router>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
